52f95144dce82a909add79cec0a9151cbbe53d36
Mohamed Lamine <fadimoh@hotmail.fr>
2022-11-22 14:35:05 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Container, Paper, styled, Grid, Card } from "@mui/material";
+
+const ResultContainer = styled("div")(({ theme }) => ({
+  width: "100%",
+  height: "2em",
+  padding: theme.spacing(2),
+  fontSize: "3em",
+  textAlign: "right",
+  overflow: "hidden",
+}));
+
+const CalculatorBase = styled(Paper)(({ theme }) => ({
+  padding: theme.spacing(2),
+  marginTop: theme.spacing(4),
+  borderRadius: 15,
+}));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="sm">
+      <CalculatorBase elevation={3}>
+        <Grid container spacing={1}>
+          <Grid item xs={12}>
+            <ResultContainer>0.0</ResultContainer>
+          </Grid>
+        </Grid>
+      </CalculatorBase>
+    </Container>
   );
 }
 
---
+++ src/theme.tsx
@@ -0,0 +1,10 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: { main: "#fff149" },
+  },
+});
+
+export default theme;
