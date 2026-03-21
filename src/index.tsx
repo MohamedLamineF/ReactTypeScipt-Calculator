60aeccca286a564ebd5cf1a2337a95d9f6e0f7d2
Mohamed Lamine FADIS <15929568+MohamedLamineF@users.noreply.github.com>
2026-03-21 14:09:53 +0100

    feat: update project title and entry point, integrate Tailwind CSS

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,10 @@
-import { ThemeProvider } from "@emotion/react";
-import { CssBaseline } from "@mui/material";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import theme from "./theme";
+import "./index.css";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
-root.render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>
+    <App />
+  </React.StrictMode>,
 );
